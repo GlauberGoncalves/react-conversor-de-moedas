901e52268b015989ca8dfa8d434a39c6d7701a3e
Glauber Goncalves <sistemas.glauber@gmail.com>
2020-01-19 12:27:32 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import Conversor from './components/conversor'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Conversor de moedas</h1>
+      
+      <div className="linha">
+        <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+        <Conversor moedaA="BRL" moedaB="USD"></Conversor>
+      </div>
+
+      <div className="linha">
+        <Conversor moedaA="ARS" moedaB="BRL"></Conversor>
+        <Conversor moedaA="BRL" moedaB="ARS"></Conversor>
+      </div>
+
+      <div className="linha">
+        <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+        <Conversor moedaA="BRL" moedaB="USD"></Conversor>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
